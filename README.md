865a901b7b89f0e456b061c539afaf958d477062
iderobina1 <134453325+iderobina1@users.noreply.github.com>
2023-07-14 18:37:07 -0600

    Adding saved message after saving, fixing local storage save for each event.

--- README.md
+++ README.md
@@ -1,28 +1,43 @@
-# workdayscheduler
+# Work Day Schedule
 
+Work Day Schedule is a simple web application that allows you to schedule and manage your daily tasks. It provides an intuitive interface where you can enter and save your agenda for each hour of the work day. The application also dynamically updates the styling of each time block based on the current hour.
 
-Web Application that Allows you to plan your work day- (9-5) by scheduling events.
 
 ## Features
 
-- Display of current date and time in eaach block.
-- Saving events to local storage
+- Display of current date and time.
+- Time blocks for each hour of the work day.
+- Ability to enter and save agenda items for each time block.
+- Dynamic styling of time blocks based on the current hour.
 
 ## Technologies Used
 
 - HTML
 - CSS
 - JavaScript
-- Bootsrap
-- Jquery
-- Day.JS
+- [Bootstrap](https://getbootstrap.com/) framework
+- [jQuery](https://jquery.com/) library
+- [Day.js](https://day.js.org/) library
 
+## Getting Started
+
+To use the Work Day Schedule application, simply visit the [live demo](https://nagaft.github.io/Work_Day_Schedule/) in your web browser. The page will load with the current date and time displayed, along with time blocks for each hour of the work day.
 
 ### Usage
 
-1. Click on the block and type your event .
-2. Click save to save the event.
+1. Click on a time block to enter your agenda for that hour.
+2. Press the Enter key or click outside the time block to save your agenda.
+3. Your agenda will be saved and will persist even if you refresh the page.
+4. The styling of each time block will automatically update based on the current hour.
+
+## Contributing
+
+Contributions to the Work Day Schedule project are welcome! If you have any suggestions, bug reports, or feature requests, please submit them as issues or pull requests on the [GitHub repository](https://github.com/YourGitHubUsername/Work_Day_Schedule).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
 
 ## Acknowledgments
 
-Bootcamp by edx
+This project was inspired by the need for a simple and user-friendly daily task scheduler. Special thanks to the contributors of the Bootstrap, jQuery, and Day.js libraries for their valuable tools and resources.
--- script.js
+++ script.js
@@ -1,104 +1,43 @@
-currentDay = document.getElementById('currentDay');
-currentDay.textContent = dayjs().format('dddd, MMMM D');
 
-container = document.querySelector('.container-lg');
+$(document).ready(function() {
+  setInterval(function() {
+    var updatedDate = dayjs().format('DD-MMM-YYYY HH:mm:ss');
+    $('#currentDay').text(updatedDate);
 
-function sunMoon(hour) {
-  amPm = 'AM';
-  if (hour >= 12) {
-    amPm = 'PM';
+    var currentHour = dayjs().hour();
+    $('.row.time-block').each(function() {
+      var hourId = parseInt($(this).attr('id').split('-')[1]);
+
+      $(this).removeClass('past present future');
+      if (hourId < currentHour) {
+        $(this).addClass('past');
+      } else if (hourId === currentHour) {
+        $(this).addClass('present');
+      } else {
+        $(this).addClass('future');
+      }
+    });
+  }, 1000);
+
+  $('.row.time-block').each(function() {
+    var hourId = parseInt($(this).attr('id').split('-')[1]);
+    var scheduleData = localStorage.getItem('schedule-' + hourId);
+
+    if (scheduleData) {
+      $(this).find('.description').val(scheduleData);
+    }
+  });
+
+  $('.time-block .saveBtn').click(function() {
+    var row = $(this).closest('.row.time-block');
+    var hourId = parseInt(row.attr('id').split('-')[1]);
+    var newText = row.find('.description').val();
+
+    row.find('.description').text(newText);
+    saveAgendaData(hourId, newText);
+  });
+
+  function saveAgendaData(hourId, text) {
+    localStorage.setItem('schedule-' + hourId, text);
   }
-  return hour + amPm;
-}
-
-for (let hour = 9; hour <= 17; hour++) {
-
-  div = document.createElement('div');
-  div.classList.add('row', 'time-block');
-
-  time = document.createElement('div');
-  time.classList.add('col-2', 'col-md-1', 'hour', 'text-center', 'py-3');
-  time.textContent = sunMoon(hour);
-
-  textarea = document.createElement('textarea');
-  textarea.classList.add('col-8', 'col-md-10', 'description');
-  textarea.setAttribute('rows', 2);
-
-  button = document.createElement('button');
-  button.classList.add('btn', 'saveBtn', 'col-2', 'col-md-1');
-  button.setAttribute('aria-label', 'save');
-
-  icon = document.createElement('i');
-  icon.classList.add('fas', 'fa-save');
-  icon.setAttribute('aria-hidden', true);
-
-  button.appendChild(icon);
-
-  div.appendChild(time);
-  div.appendChild(textarea);
-  div.appendChild(button);
-
-  container.appendChild(div);
-
-}
-
-function colorCode() {
-
-  hours = document.querySelectorAll('.time-block');
-
-  hours.forEach (hour => {
-
-    id = hour.id;
-    hourInt = parseInt(id.split('-')[1]);
-
-    now = dayjs().hour();
-
-    if (hourInt < now) {
-      hour.classList.add('past');
-    } else if (hourInt === now) {
-      hour.classList.add('present'); 
-    } else {
-      hour.classList.add('future');
-    }
-
-  });
-
-}
-
-// Color code on load
-colorCode();
-
-// Save events
-saveBtns = document.querySelectorAll('.saveBtn');
-
-saveBtns.forEach(btn => {
-
-  btn.addEventListener('click', function(e) {
-
-    timeblock = e.target.parentElement;
-    id = timeblock.id;
-    text = timeblock.querySelector('.description').value;
-
-    localStorage.setItem(id, text);
-
-  });
-
-});
-
-// Load saved events
-document.addEventListener('DOMContentLoaded', function() {
-
-  timeblocks = document.querySelectorAll('.time-block');
-
-  timeblocks.forEach(block => {
-
-    id = block.id;
-    text = localStorage.getItem(id);
-
-    if (text) {
-      block.querySelector('.description').value = text;
-    }
-
-  });
-
 });
